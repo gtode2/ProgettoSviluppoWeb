ad4b4b538e8f3be673ad121e73bd9a501ccf6f4f
Gabriele <gabry21102002.0@gmail.com>
2025-06-07 12:19:37 +0200

    b

--- Frontend/login/login.js
+++ Frontend/login/login.js
@@ -29,20 +29,18 @@ document.addEventListener("DOMContentLoaded", ()=>{
             
             })
             const data = await response.json()
-            console.log("AAAAA");
             
             if (!response.ok) {
               if (response.status===403) {
                 window.location.href = "/ban"
               }
-              else{
+              else if (response.status===401) {
                 alert("Credenziali errate")
               }
             }else{
                 console.log("tentativo di href");
                 
                 window.location.href = "/"
-                //redirect a homepage
             }
         } catch (err) {
             console.log(err);
@@ -52,29 +50,3 @@ document.addEventListener("DOMContentLoaded", ()=>{
 })
 
 
-        
-/*
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("send");
-
-  btn.addEventListener("click", () => {
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-
-    if (!username || !password) {
-      alert("⚠️ Inserisci entrambi i campi.");
-      return;
-    }
-
-    // Simulazione accesso
-    if (username === "admin" && password === "admin123") {
-      alert("Accesso Admin effettuato!");
-      // window.location.href = "admin.html";
-    } else {
-      alert(`✅ Bentornato, ${username}`);
-      // window.location.href = "cliente.html" o "artigiano.html";
-    }
-  });
-});
-
-*/
