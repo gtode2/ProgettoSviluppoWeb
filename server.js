3aab3de5826340bc1e25c5911d147dee84e91d04
Gabriele <gabry21102002.0@gmail.com>
2025-06-08 18:15:01 +0200

    rimosso script db

--- server.js
+++ server.js
@@ -49,31 +49,16 @@ async function main() {
         express.json()(req, res, next);
       }
     });
-    const pool = await initDb()
 
-    // Connessione PostgreSQL
-    async function initDb() {
-        var cdb = await checkdb()
-        if (cdb===0) {
-            console.log("Creazione pool");
+    const pool = new Pool({
+        user: process.env.DB_USER,
+        host: "postgres",
+        database: process.env.DB_NAME,
+        password: process.env.DB_PW,
+        port: process.env.DB_PORT,
+    });
 
-            const pool = new Pool({
-            user: process.env.DB_USER,
-            host: "postgres",
-            database: process.env.DB_NAME,
-            password: process.env.DB_PW,
-            port: process.env.DB_PORT,
-            });
-            console.log("Pool creata correttamente");
-            return pool
 
-        }else{
-            console.log("impossibile inizializzare DB");
-            console.log(cdb);
-        
-            process.exit(0)
-        }
-    }
     cron.schedule('* * * * *', async() => {
         //rimozione token scaduti
         try {
